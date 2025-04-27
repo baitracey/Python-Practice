9cd07dd9ccd2e78fc169c1d1ea76abf3edd90f10
Bailey Tracey <baileytracey2003@gmail.com>
2025-04-28 00:01:06 +0100

    Add project files.

---
+++ Python Practice/Python_Practice.py
@@ -0,0 +1,9 @@
+# Define the release and runtime integer variables below:
+release_year = 2023
+runtime = 205
+an_int = 2024
+
+# Define the rating_out_of_10 float variable below: 
+print(an_int + 7)
+rating_out_of_10 = 7.2
+a_float = 2.8
