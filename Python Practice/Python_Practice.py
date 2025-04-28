e1f5a826e7ab6dd8476ab9fef054f1359716ad07
Bailey Tracey <baileytracey2003@gmail.com>
2025-04-28 01:33:23 +0100

    Hello World Modulo Python Lesson

--- Python Practice/Python_Practice.py
+++ Python Practice/Python_Practice.py
@@ -55,5 +55,13 @@ print(7 ** 2)
 # 8x8 quilt
 print(8 ** 2)
 
+first_order_remainder = 269 % 10
+print(first_order_remainder)
 
+first_order_coupon = 'no'
+
+second_order_remainder = 270 % 10
+print(second_order_remainder)
+
+second_order_coupon = 'yes'
 
